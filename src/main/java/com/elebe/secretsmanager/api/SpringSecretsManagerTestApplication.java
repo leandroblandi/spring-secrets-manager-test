30c9981596d1d49c9854919ab668f63e754f6b14
Leandro Blandi <leandronblandi@gmail.com>
2025-11-03 15:32:04 -0300

    feat: initial commit

---
+++ src/main/java/com/elebe/secretsmanager/api/SpringSecretsManagerTestApplication.java
@@ -0,0 +1,13 @@
+package com.elebe.secretsmanager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecretsManagerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecretsManagerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elebe/secretsmanager/api/SpringSecretsManagerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elebe.secretsmanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecretsManagerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
