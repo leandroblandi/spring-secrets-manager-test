29ef89e6385eaef0b8e15c775628c7b8191b2cdf
Leandro Blandi <leandronblandi@gmail.com>
2025-11-03 20:56:57 -0300

    feat: added cache for secrets

--- src/main/java/com/elebe/secretsmanager/api/model/CachedSecret.java
+++ src/main/java/com/elebe/secretsmanager/api/model/CachedSecret.java
@@ -1,4 +1,24 @@
 package com.elebe.secretsmanager.api.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Builder
+@Getter
 public class CachedSecret {
+    private String secretValue;
+    private Instant cacheTime;
+
+    public static CachedSecret create(String secretValue) {
+        return CachedSecret.builder()
+                .secretValue(secretValue)
+                .cacheTime(Instant.now())
+                .build();
+    }
+
+    public boolean isExpired() {
+        return cacheTime.plusSeconds(60).isBefore(Instant.now());
+    }
 }
--- src/main/java/com/elebe/secretsmanager/api/service/SecretsManagerServiceImpl.java
+++ src/main/java/com/elebe/secretsmanager/api/service/SecretsManagerServiceImpl.java
@@ -1,43 +1,56 @@
 package com.elebe.secretsmanager.api.service;
 
+import com.elebe.secretsmanager.api.model.CachedSecret;
 import com.elebe.secretsmanager.api.request.SaveSecretRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 @Service
 public class SecretsManagerServiceImpl implements SecretsManagerService {
     private final SecretsManagerClient secretsManagerClient;
+    private final Map<String, CachedSecret> cachedSecrets;
 
     public SecretsManagerServiceImpl(SecretsManagerClient secretsManagerClient) {
         this.secretsManagerClient = secretsManagerClient;
+        this.cachedSecrets = new ConcurrentHashMap<>();
     }
 
     @Override
     public void saveSecret(SaveSecretRequest request) {
         log.debug("Saving secret with key '{}'.", request.getSecretName());
 
-        try {
-            secretsManagerClient.createSecret(builder -> builder
-                    .name(request.getSecretName())
-                    .secretString(request.getSecretValue())
-                    .description("Secret created by Spring Secrets Manager Test Application.")
-            );
-            log.debug("Secret with key '{}' saved successfully.", request.getSecretName());
-        } catch (Exception e) {
-            throw new RuntimeException("An error occurred while saving secret with key '%s'."
-                    .formatted(request.getSecretName()), e);
-        }
+        secretsManagerClient.createSecret(builder -> builder
+                .name(request.getSecretName())
+                .secretString(request.getSecretValue())
+                .description("Secret created by Spring Secrets Manager Test Application.")
+        );
+
+        log.debug("Secret with key '{}' saved successfully.", request.getSecretName());
+        saveSecretInCache(request.getSecretName(), request.getSecretValue());
     }
 
     @Override
     public String getSecret(String secretName) {
         log.debug("Getting secret with key '{}'.", secretName);
-        try {
-            return secretsManagerClient.getSecretValue(builder -> builder.secretId(secretName)).secretString();
-        } catch (Exception e) {
-            throw new RuntimeException("An error occurred while getting secret with key '%s'.".formatted(secretName), e);
+
+        if (cachedSecrets.containsKey(secretName) && !cachedSecrets.get(secretName).isExpired()) {
+            log.debug("Secret with key '{}' is in cache, returning it...", secretName);
+            return cachedSecrets.get(secretName).getSecretValue();
         }
+
+        log.debug("Cache with key '{}' is not in cache, fetching it from AWS Secrets Manager...", secretName);
+        String secretValue = secretsManagerClient.getSecretValue(builder -> builder.secretId(secretName)).secretString();
+
+        saveSecretInCache(secretName, secretValue);
+        return secretValue;
+    }
+
+    private void saveSecretInCache(String secretName, String secretValue) {
+        cachedSecrets.putIfAbsent(secretName, CachedSecret.create(secretValue));
     }
 }
